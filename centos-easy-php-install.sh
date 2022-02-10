dbb644482787cad0ac56b0fd7a70d09692c50fb7
Meysam Eradeshahi <eradeshahi@gmail.com>
2022-02-10 21:16:46 +0330

    add composer
    install composer

--- centos-easy-php-install.sh
+++ centos-easy-php-install.sh
@@ -1,11 +1,6 @@
 # get centos version
 version=$(rpm -q --queryformat '%{VERSION}' centos-release)
 
-
-
-
-# While : Do
-
 while :
 do
   read -p "Enter php version (example:7.4) : " PHP_VERSION
@@ -16,6 +11,14 @@ do
   fi
 done
 
+
+CURRENTPHP=$(php -r "echo substr(phpversion(),0,3);")
+
+if [[  $CURRENTPHP =~ $PHP_VERSION ]]
+then
+  echo "php $PHP_VERSION already installed."
+  exit
+fi
 
 
 
@@ -33,3 +36,17 @@ else
 fi
 sudo yum -y remove php  php-*
 sudo yum -y install php php-{cli,fpm,mysqlnd,zip,devel,gd,mbstring,curl,xml,pear,bcmath,json}
+
+## install composer
+composer -v > /dev/null 2>&1
+COMPOSER=$?
+if [[ $COMPOSER -ne 0 ]]; then
+    sudo yum -y install wget unzip
+
+  php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+  php -r "if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+  php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+  php -r "unlink('composer-setup.php');"
+
+  
+fi
